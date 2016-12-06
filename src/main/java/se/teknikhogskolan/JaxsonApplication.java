29281d3cbc1e7fb622b27120544d506e1218490a
Johan Åsling <johan.asling@hotmail.com>
2016-12-06 13:20:42 +0100

    commit

---
+++ src/main/java/se/teknikhogskolan/JaxsonApplication.java
@@ -0,0 +1,12 @@
+package se.teknikhogskolan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxsonApplication.class, args);
+	}
+}
