a506f4056ccbad110f0789a3ee5b069956903b90
Johan Åsling <johan.asling@hotmail.com>
2016-12-06 13:30:38 +0100

    Testing

--- src/main/java/se/teknikhogskolan/JaxsonApplication.java
+++ src/main/java/se/teknikhogskolan/JaxsonApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JaxsonApplication {
 
+    //Test
 	public static void main(String[] args) {
 		SpringApplication.run(JaxsonApplication.class, args);
 	}
