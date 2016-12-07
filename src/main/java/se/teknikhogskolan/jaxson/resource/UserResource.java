9bfda8824ac7a0edae6f4ba2ecf48d3eb3c8b4ac
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2016-12-07 14:19:00 +0100

    Created the foundation of the UserResource Interface and created an implementation

--- src/main/java/se/teknikhogskolan/jaxson/resource/UserResource.java
+++ src/main/java/se/teknikhogskolan/jaxson/resource/UserResource.java
@@ -1,23 +1,43 @@
 package se.teknikhogskolan.jaxson.resource;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import se.teknikhogskolan.jaxson.model.UserModel;
 
+import java.util.List;
+
 @Path("users")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface UserResource {
 
     @POST
-    UserModel createUser(UserModel user);
+    Response createUser(UserModel user);
+
+    @GET
+    @Path("{userNumber}")
+    UserModel getUserByUserNumber(@PathParam("userNumber") Long userNumber);
 
     @PUT
     @Path("{userNumber}")
     UserModel updateUser(@PathParam("userNumber") Long userNumber, UserModel user);
 
+    @GET
+    List<UserModel> getUserByParameter(@QueryParam("username") String username, @QueryParam("firstname") String firstname,
+                                       @QueryParam("lastname") String lastname);
+
+    /* TODO
+    *  Activate and inactive user
+    *  Get all by page
+    *  Get all by creationDate
+    * */
 }
--- src/main/java/se/teknikhogskolan/jaxson/resource/implementation/UserResourceImpl.java
+++ src/main/java/se/teknikhogskolan/jaxson/resource/implementation/UserResourceImpl.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import se.teknikhogskolan.jaxson.model.UserModel;
 import se.teknikhogskolan.jaxson.resource.UserResource;
 import se.teknikhogskolan.springcasemanagement.model.User;
+import se.teknikhogskolan.springcasemanagement.service.TeamService;
 import se.teknikhogskolan.springcasemanagement.service.UserService;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 public class UserResourceImpl implements UserResource{
 
@@ -13,16 +17,22 @@ public class UserResourceImpl implements UserResource{
     UserService userService;
 
     @Override
-    public UserModel createUser(UserModel user) {
-        User createdUser = userService.create(user.getUserNumber(), user.getUsername(),
-                user.getFirstName(), user.getLastName());
+    public Response createUser(UserModel user) {
+        return null;
+    }
 
-        return new UserModel(createdUser.getUserNumber(), createdUser.getUsername(),
-                createdUser.getFirstName(), createdUser.getLastName());
+    @Override
+    public UserModel getUserByUserNumber(Long userNumber) {
+        return null;
     }
 
     @Override
     public UserModel updateUser(Long userNumber, UserModel user) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> getUserByParameter(String username, String firstname, String lastname) {
         return null;
     }
 }
