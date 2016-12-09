06b53a68dde9b7cbee998093fa7fb4bdca7f120b
Johan Åsling <johan.asling@hotmail.com>
2016-12-09 10:41:20 +0100

    Added no search result exception mapper

--- src/main/java/se/teknikhogskolan/jaxson/config/JerseyConfig.java
+++ src/main/java/se/teknikhogskolan/jaxson/config/JerseyConfig.java
@@ -1,23 +1,25 @@
 package se.teknikhogskolan.jaxson.config;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import se.teknikhogskolan.jaxson.exception.NoSearchResultExceptionMapper;
 import se.teknikhogskolan.jaxson.resource.implementation.IssueResourceImpl;
 import se.teknikhogskolan.jaxson.resource.implementation.TeamResourceImpl;
 import se.teknikhogskolan.jaxson.resource.implementation.UserResourceImpl;
 import se.teknikhogskolan.jaxson.resource.implementation.WorkItemResourceImpl;
 
-import javax.ws.rs.ApplicationPath;
-
 @Component
 @ApplicationPath("jaxson")
-public final class JerseyConfig extends ResourceConfig{
+public final class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(TeamResourceImpl.class);
         register(IssueResourceImpl.class);
         register(UserResourceImpl.class);
         register(WorkItemResourceImpl.class);
+        register(NoSearchResultExceptionMapper.class);
     }
 }
