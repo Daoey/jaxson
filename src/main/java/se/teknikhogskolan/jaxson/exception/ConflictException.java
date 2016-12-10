cde17e9db687b44bb3f97b5c09ddcff9063e76e5
Johan Åsling <johan.asling@hotmail.com>
2016-12-10 14:36:54 +0100

    Created conflict exception

--- src/main/java/se/teknikhogskolan/jaxson/exception/ConflictException.java
+++ src/main/java/se/teknikhogskolan/jaxson/exception/ConflictException.java
@@ -1,5 +1,18 @@
 package se.teknikhogskolan.jaxson.exception;
 
-public class ConflictException {
+public class ConflictException extends RuntimeException {
 
+    private static final long serialVersionUID = 7968389335316189609L;
+
+    protected ConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    protected ConflictException(String message) {
+        super(message);
+    }
+
+    protected ConflictException() {
+        super();
+    }
 }
