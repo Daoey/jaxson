47eb8070b311daa31fe32e9392fc08eae205cda8
Johan Åsling <johan.asling@hotmail.com>
2016-12-10 14:38:52 +0100

    Made ConflictException constructors public

--- src/main/java/se/teknikhogskolan/jaxson/exception/ConflictException.java
+++ src/main/java/se/teknikhogskolan/jaxson/exception/ConflictException.java
@@ -4,15 +4,15 @@ public final class ConflictException extends RuntimeException {
 
     private static final long serialVersionUID = 7968389335316189609L;
 
-    protected ConflictException(String message, Throwable cause) {
+    public ConflictException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    protected ConflictException(String message) {
+    public ConflictException(String message) {
         super(message);
     }
 
-    protected ConflictException() {
+    public ConflictException() {
         super();
     }
 }
