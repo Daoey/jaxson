ecf1cdf7c34233aa9dc337a74559e6046ef777e0
peferb <pe.ferb@gmail.com>
2016-12-13 21:06:48 +0100

    Get Team test checks response status code and Team id and Team name

--- src/test/java/se/teknikhogskolan/resource/TestTeamResource.java
+++ src/test/java/se/teknikhogskolan/resource/TestTeamResource.java
@@ -4,28 +4,36 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import se.teknikhogskolan.model.TeamViewBean;
 
-import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestTeamResource {
-    private static Client client;
-    private static final String webAppUrl = "http://localhost:8080/jaxson";
-    private static final String resource = "teams";
+    private static WebTarget teamWebTarget;
 
     @BeforeClass
     public static void masterSetup() {
-        client = ClientBuilder.newClient();
+        String webAppUrl = "http://localhost:8080/jaxson";
+        String resource = "teams";
+        teamWebTarget = ClientBuilder.newClient().target(webAppUrl).path(resource);
     }
 
     @Test
     public void canGetTeamById() {
         Long teamId = 14L;
-        WebTarget target = client.target(webAppUrl).path(resource).path(teamId.toString());
-        TeamViewBean teamViewBean = target.request(MediaType.APPLICATION_JSON_TYPE).get(TeamViewBean.class);
+        Response response = teamWebTarget.path(teamId.toString()).request(MediaType.APPLICATION_JSON_TYPE).get();
+        assertEquals(OK, response.getStatusInfo());
+        TeamViewBean teamViewBean = response.readEntity(TeamViewBean.class);
         assertEquals(teamId, teamViewBean.getId());
+        assertNotEmpty(teamViewBean.getName());
+    }
+
+    private static void assertNotEmpty(String string) {
+        if (null == string || string.isEmpty()) fail();
     }
 }
