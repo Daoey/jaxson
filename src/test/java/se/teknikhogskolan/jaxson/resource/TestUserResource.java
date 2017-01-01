c246536315bb645cc0a3a6c947de0849da755ab0
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2017-01-01 19:37:34 +0100

    Added the TestUserResource from master to dev and added SQL script to insert and delete data from the database

--- src/test/java/se/teknikhogskolan/jaxson/resource/TestUserResource.java
+++ src/test/java/se/teknikhogskolan/jaxson/resource/TestUserResource.java
@@ -17,6 +17,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 import se.teknikhogskolan.jaxson.JaxsonApplication;
 import se.teknikhogskolan.jaxson.model.UserDto;
@@ -25,15 +27,18 @@ import se.teknikhogskolan.springcasemanagement.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = JaxsonApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = {HsqlInfrastructureConfig.class})
-public class TestUserResource {
-    @LocalServerPort
-    private int randomPort;
-    private static final String auth = "Authorization";
-    private static final String authCode = "Basic cm9vdDpzZWNyZXQ=";
+@ContextConfiguration(classes = { HsqlInfrastructureConfig.class })
+@SqlGroup({ @Sql(scripts = "insert_user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(scripts = "hsql_clean_tables.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD) })
+public final class TestUserResource {
+
     private static Client client;
     private String baseUrl;
     private UserDto userInDb;
+    private static final String auth = "Authorization";
+    private static final String authCode = "Basic cm9vdDpzZWNyZXQ=";
+    @LocalServerPort
+    private int randomPort;
 
     @BeforeClass
     public static void initialize() {
@@ -43,6 +48,15 @@ public class TestUserResource {
     @Before
     public void setUp() {
         baseUrl = String.format("http://localhost:%d/jaxson/", randomPort);
+        userInDb = new UserDto(new User(1L, "Robotarm Luke", "Luke", "Skywalker"));
+    }
+
+    @Test
+    public void canCreateUser() throws Exception {
+        Response response = client.target(baseUrl).path("users").request().header(auth, authCode)
+                .post(Entity.entity(new User(457L, "canCreateUser", "temp", "temp"), MediaType.APPLICATION_JSON));
+
+        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
     }
 
     @Test
@@ -64,13 +78,7 @@ public class TestUserResource {
         // client.target(baseUrl).path("users/1").request().put();
     }
 
-    @Test
-    public void canCreateUser() throws Exception {
-        Response response = client.target(baseUrl).path("users").request().header(auth, authCode)
-                .post(Entity.entity(new User(457L, "canCreateUser", "temp", "temp"), MediaType.APPLICATION_JSON));
 
-        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
-    }
 
     @Test
     public void getAllWorkItemsFromUserWithoutWorkItemsReturnsNotFound() {
