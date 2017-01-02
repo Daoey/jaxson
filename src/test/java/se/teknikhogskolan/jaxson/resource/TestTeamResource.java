094ed9ddc525687d0c7d8b423d0710234cb9e6e3
peferb <pe.ferb@gmail.com>
2017-01-02 18:16:38 +0100

    Using .sql adding test data to TestTeamResource

--- src/test/java/se/teknikhogskolan/jaxson/resource/TestTeamResource.java
+++ src/test/java/se/teknikhogskolan/jaxson/resource/TestTeamResource.java
@@ -1,42 +1,62 @@
 package se.teknikhogskolan.jaxson.resource;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import se.teknikhogskolan.jaxson.JaxsonApplication;
-import se.teknikhogskolan.jaxson.model.TeamDto;
-import se.teknikhogskolan.springcasemanagement.config.h2.H2InfrastructureConfig;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.OK;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.URI;
 
-import static javax.ws.rs.core.Response.Status.CREATED;
-import static javax.ws.rs.core.Response.Status.OK;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+import se.teknikhogskolan.jaxson.JaxsonApplication;
+import se.teknikhogskolan.jaxson.model.TeamDto;
+import se.teknikhogskolan.jaxson.model.UserDto;
+import se.teknikhogskolan.springcasemanagement.config.hsql.HsqlInfrastructureConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = JaxsonApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = {H2InfrastructureConfig.class})
+@ContextConfiguration(classes = {HsqlInfrastructureConfig.class})
+@SqlGroup({
+        @Sql(scripts = "insert_team.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(scripts = "hsql_clean_tables.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)})
 public class TestTeamResource {
 
-    @LocalServerPort private int randomPort;
-    private static final String auth = "Authorization";
-    private static final String authCode = "Basic cm9vdDpzZWNyZXQ=";
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String AUTHORIZATION_CODE = "Basic cm9vdDpzZWNyZXQ=";
     private static Client client;
+    @Rule public ExpectedException thrown = ExpectedException.none();
+    @LocalServerPort private int randomPort;
     private WebTarget teamWebTarget;
     private URI teamInDbLocation;
-    private TeamDto teamDto;
+
+    // Data from insert_team.sql
+    private final Long turtlesTeamId = 2001L;
+    private final Long clanTeamId = 2002L;
+    private TeamDto turtlesTeam;
+    private TeamDto clanTeam;
 
     @BeforeClass
     public static void initialize() {
@@ -48,24 +68,93 @@ public class TestTeamResource {
         String targetUrl = String.format("http://localhost:%d/jaxson/", randomPort);
         String resource = "teams";
         teamWebTarget = client.target(targetUrl).path(resource);
-        teamDto = new TeamDto("Testing Team");
-        teamInDbLocation = teamWebTarget.request().header(auth, authCode)
-                .post(Entity.entity(teamDto, MediaType.APPLICATION_JSON)).getLocation();
+        getTeamsFromDatabase();
+    }
+
+    private void getTeamsFromDatabase() {
+        try { // TODO remove try-catch when possible
+            turtlesTeam = teamWebTarget.path(turtlesTeamId.toString()).request()
+                    .header(AUTHORIZATION, AUTHORIZATION_CODE).get(TeamDto.class);
+            clanTeam = teamWebTarget.path(clanTeamId.toString()).request().header(AUTHORIZATION, AUTHORIZATION_CODE)
+                    .get(TeamDto.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void createTeam() {
-        Response response = teamWebTarget.request().header(auth, authCode)
-                .post(Entity.entity(new TeamDto("Created Team"), MediaType.APPLICATION_JSON));
-        assertEquals(CREATED, response.getStatusInfo());
-        assertNotNull(response.getLocation());
+    public void getUsersFromTeamWithoutUsersShouldReturnEmptyList() {
+        Response response = teamWebTarget.path(clanTeam.getId().toString() + "/users").request()
+                .header(AUTHORIZATION, AUTHORIZATION_CODE).get();
+        assertEquals(OK, response.getStatusInfo());
+        List<UserDto> result = response.readEntity(new GenericType<List<UserDto>>(){});
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void canGetUsersFromTeam() {
+        Response response = teamWebTarget.path(turtlesTeam.getId().toString() + "/users").request()
+                .header(AUTHORIZATION, AUTHORIZATION_CODE).get();
+        assertEquals(OK, response.getStatusInfo());
+        List<UserDto> result = response.readEntity(new GenericType<List<UserDto>>(){});
+        List<String> firstNamesInResult = result.stream().map(UserDto::getFirstName).collect(Collectors.toList());
+        assertTrue(firstNamesInResult.contains("Leonardo"));
+        assertTrue(firstNamesInResult.contains("Raffaello"));
+        assertTrue(firstNamesInResult.contains("Michelangelo"));
+        assertTrue(firstNamesInResult.contains("Donatello"));
+        assertTrue(firstNamesInResult.contains("Splinter"));
+    }
+
+    @Test
+    public void canGetAllTeams() {
+        Response response = teamWebTarget.request().header(AUTHORIZATION, AUTHORIZATION_CODE).get();
+        assertEquals(OK, response.getStatusInfo());
+        List<TeamDto> result = response.readEntity(new GenericType<List<TeamDto>>(){});
+        assertTrue(result.contains(turtlesTeam));
+        assertTrue(result.contains(clanTeam));
+    }
+
+    @Test
+    @Sql("hsql_clean_tables.sql") // TODO move to mocking test
+    public void getAllTeamsShouldReturnEmptyListIfNoTeamExist() {
+        Response response = teamWebTarget.request().header(AUTHORIZATION, AUTHORIZATION_CODE).get();
+        assertEquals(OK, response.getStatusInfo());
+        List<TeamDto> result = response.readEntity(new GenericType<List<TeamDto>>(){});
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void createTeamReturnsLocation() {
+        TeamDto teamDto = new TeamDto("Testing Team");
+        teamInDbLocation = teamWebTarget.request().header(AUTHORIZATION, AUTHORIZATION_CODE)
+                .post(Entity.entity(teamDto, MediaType.APPLICATION_JSON)).getLocation();
+        assertNotNull(teamInDbLocation);
+
+        Response response = client.target(teamInDbLocation).request().header(AUTHORIZATION, AUTHORIZATION_CODE).get();
+        TeamDto result = response.readEntity(TeamDto.class);
+        assertEquals(teamDto, result);
+    }
+
+    @Test
+    public void getNotExistingTeamShouldReturn404() {
+        final String nonExistingTeam = "8489584656";
+        Response response = teamWebTarget.path(nonExistingTeam).request().header(AUTHORIZATION, AUTHORIZATION_CODE)
+                .get();
+        assertEquals(NOT_FOUND, response.getStatusInfo());
+    }
+
+    @Test
+    public void getTeamWithoutAuthorizationShouldReturn403() {
+        Response response = teamWebTarget.path(turtlesTeamId.toString()).request().get();
+        assertEquals(UNAUTHORIZED, response.getStatusInfo());
     }
 
     @Test
     public void canGetTeamById() {
-        Response response = client.target(teamInDbLocation).request().header(auth, authCode).get();
+        Response response = teamWebTarget.path(turtlesTeamId.toString()).request()
+                .header(AUTHORIZATION, AUTHORIZATION_CODE).get();
         assertEquals(OK, response.getStatusInfo());
         TeamDto result = response.readEntity(TeamDto.class);
-        assertEquals(teamDto.getName(), result.getName());
+        assertEquals(turtlesTeam.getName(), result.getName());
     }
 }
