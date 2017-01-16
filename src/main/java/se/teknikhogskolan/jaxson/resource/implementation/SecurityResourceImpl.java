077a0a694f768b3f3b0179da77a9efde0324fcdb
Martin Söderstrand <Martinsoderstrand96@hotmail.com>
2017-01-16 14:41:41 +0100

    Removed Security dependency

--- src/main/java/se/teknikhogskolan/jaxson/resource/implementation/SecurityResourceImpl.java
+++ src/main/java/se/teknikhogskolan/jaxson/resource/implementation/SecurityResourceImpl.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import service.UserService;
+import se.teknikhogskolan.springcasemanagement.service.UserService;
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
