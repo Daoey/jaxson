17f3ce510cd0913a34bd87e70f3c8c9b56e4b709
Johan <Johan@DESKTOP-TLIBQ1I>
2017-01-18 11:30:03 +0100

    Some changes to authreqfilter

--- src/main/java/se/teknikhogskolan/jaxson/security/AuthorizationRequestFilter.java
+++ src/main/java/se/teknikhogskolan/jaxson/security/AuthorizationRequestFilter.java
@@ -1,21 +1,18 @@
 package se.teknikhogskolan.jaxson.security;
 
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import se.teknikhogskolan.jaxson.exception.ErrorMessage;
 import se.teknikhogskolan.springcasemanagement.model.SecurityUser;
 import se.teknikhogskolan.springcasemanagement.service.SecurityUserService;
+import se.teknikhogskolan.springcasemanagement.service.exception.NotAuthorizedException;
 
 public class AuthorizationRequestFilter implements ContainerRequestFilter {
 
@@ -34,9 +31,7 @@ public class AuthorizationRequestFilter implements ContainerRequestFilter {
         String authorizationHeader = requestContext.getHeaderString("Authorization");
 
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            requestContext.abortWith(
-                    Response.status(Response.Status.UNAUTHORIZED).entity(new ErrorMessage(UNAUTHORIZED.getStatusCode(),
-                            UNAUTHORIZED.toString(), "Authorization header must be provided")).build());
+            throw new NotAuthorizedException("Authorization header must be provided");
         }
 
         String token = authorizationHeader.substring("Bearer".length()).trim();
@@ -49,18 +44,14 @@ public class AuthorizationRequestFilter implements ContainerRequestFilter {
         SecurityUser securityUser = securityUserService.getByToken(token);
 
         if (securityUser == null) {
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
-                    .entity(new ErrorMessage(UNAUTHORIZED.getStatusCode(), UNAUTHORIZED.toString(), "Not authorized"))
-                    .build());
+            throw new NotAuthorizedException("Not authorized");
         }
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
         LocalDateTime date = LocalDateTime.parse(securityUser.getTokensExpiration().get(token), formatter);
 
         if (date.isBefore(LocalDateTime.now())) {
-            requestContext.abortWith(
-                    Response.status(Response.Status.UNAUTHORIZED).entity(new ErrorMessage(UNAUTHORIZED.getStatusCode(),
-                            UNAUTHORIZED.toString(), "Login session has expired")).build());
+            throw new NotAuthorizedException("Login session has expired");
         }
     }
 }
