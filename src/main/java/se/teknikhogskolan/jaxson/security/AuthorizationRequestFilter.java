a6dc4c96cee0e8501f8f2dbda8e0668aa9995a5c
Johan <Johan@DESKTOP-TLIBQ1I>
2017-01-18 10:57:59 +0100

    Valididation for token implemented

--- src/main/java/se/teknikhogskolan/jaxson/security/AuthorizationRequestFilter.java
+++ src/main/java/se/teknikhogskolan/jaxson/security/AuthorizationRequestFilter.java
@@ -3,6 +3,8 @@ package se.teknikhogskolan.jaxson.security;
 import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -12,6 +14,7 @@ import javax.ws.rs.core.UriInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import se.teknikhogskolan.jaxson.exception.ErrorMessage;
+import se.teknikhogskolan.springcasemanagement.model.SecurityUser;
 import se.teknikhogskolan.springcasemanagement.service.SecurityUserService;
 
 public class AuthorizationRequestFilter implements ContainerRequestFilter {
@@ -38,18 +41,26 @@ public class AuthorizationRequestFilter implements ContainerRequestFilter {
 
         String token = authorizationHeader.substring("Bearer".length()).trim();
 
-        try {
-            validateToken(token);
-        } catch (Exception e) {
+        validateToken(token, requestContext);
+    }
+
+    private void validateToken(String token, ContainerRequestContext requestContext) {
+        // Check if it was issued by the server and if it's not expired
+        SecurityUser securityUser = securityUserService.getByToken(token);
+
+        if (securityUser == null) {
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
                     .entity(new ErrorMessage(UNAUTHORIZED.getStatusCode(), UNAUTHORIZED.toString(), "Not authorized"))
                     .build());
         }
-    }
 
-    private void validateToken(String token) throws Exception {
-        // Check if it was issued by the server and if it's not expired
-        // Throw an Exception if the token is invalid
-        // securityUserService.validate(token);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        LocalDateTime date = LocalDateTime.parse(securityUser.getTokensExpiration().get(token), formatter);
+
+        if (date.isBefore(LocalDateTime.now())) {
+            requestContext.abortWith(
+                    Response.status(Response.Status.UNAUTHORIZED).entity(new ErrorMessage(UNAUTHORIZED.getStatusCode(),
+                            UNAUTHORIZED.toString(), "Login session has expired")).build());
+        }
     }
 }
